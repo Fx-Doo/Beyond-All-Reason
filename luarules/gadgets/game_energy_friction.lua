b51a6f29c7e2e8350d3879ffc83635b923e1e1cb
Fx-Doo <hedibertille93@gmail.com>
2025-10-05 21:58:06 +0200

    Update game_energy_friction.lua

--- luarules/gadgets/game_energy_friction.lua
+++ luarules/gadgets/game_energy_friction.lua
@@ -11,12 +11,27 @@ end
 
 local sizeX = Game.mapSizeX
 local sizeZ = Game.mapSizeZ
-local sqr = 64
+local sqr = 8
 
 local nCol = sizeX / sqr
 local nRows = sizeZ / sqr
 
-local nCells = nCol * nRows
+local nCells = nCol * nRows -- use finer grain if the map is small, lower res if the map is big, so that we're never over 128*128 cells total
+while nCells > 128*128 do
+	sqr = sqr * 2
+	nCol = sizeX / sqr
+	nRows = sizeZ / sqr
+	nCells = nCol * nRows
+end
+
+local BLKSIZEX = sqr/sizeX
+local BLKSIZEZ = sqr/sizeZ
+
+function gadget:Initialize()
+	Spring.Echo(sqr, nCells)
+end
+
+	
 local energyProdPerCell = {}
 local cellTaxValue = {}
 
@@ -41,6 +56,14 @@ for defID, defs in pairs (UnitDefs) do
 	end
 end
 
+local distTable = {}
+for dx = 0,nCol do
+	distTable[dx] = {}
+	for dz = 0, nRows do
+		distTable[dx][dz] = math.sqrt((dx*sqr)^2 + (dz*sqr)^2)
+	end
+end
+
 
 local function GetTaxValue(dist)
 	local taxValue = math.min(0.5, math.max(0,(dist - taxStartDist) * taxPerDistConst)) -- clamped 0 - 0.5; value 0 at dist 256 and under, value 0.5 at dist 4096 and over, linear in between
@@ -48,7 +71,8 @@ local function GetTaxValue(dist)
 end
 
 local function CellsDist(col1, row1, col2, row2)
-	local dist = math.sqrt((sqr*(col2-col1))^2 + (sqr*(row2-row1))^2)
+	local dx, dz = math.abs(col2-col1), math.abs(row2-row1)
+	local dist = distTable[dx][dz]
 	return dist
 end
 
@@ -122,17 +146,60 @@ function gadget:UnitDestroyed(unitID, unitDefID, team)
 	end
 end
 
+function gadget:UnitGiven(unitID, unitDefID, newTeam, oldTeam)
+	if energyBuilding[unitDefID] then
+		local amount = energyBuilding[unitDefID]
+		local x,_,z = Spring.GetUnitPosition(unitID)
+		AddCellValue(x,z,newTeam, amount)
+		AddCellValue(x,z,oldTeam, -amount)
+		FlushCellTaxValues(oldTeam)
+		FlushCellTaxValues(newTeam)
+	end
+end
+
 function gadget:AllowUnitBuildStep(builderID, builderTeam, unitID, unitDefID, part)
 	local _,_,_,_,bProgress = Spring.GetUnitHealth (unitID)
 	if part > 0 and bProgress < 1 then -- positive progress on unfinished building = build
 		local x,_,z = Spring.GetUnitPosition(unitID)
 		local uCol, uRow = math.floor(x/sqr), math.floor(z/sqr)
-		local meanDist = GetMeanDist(uCol, uRow, builderTeam)
-		local taxValue = cellTaxValue[builderTeam] and cellTaxValue[builderTeam][uCol] and cellTaxValue[builderTeam][uCol][uRow] or GetTaxValue(meanDist)
-		RegisterCellTaxValue(uCol, uRow, builderTeam, taxValue)
+		local taxValue = cellTaxValue[builderTeam] and cellTaxValue[builderTeam][uCol] and cellTaxValue[builderTeam][uCol][uRow]
+		if not taxValue then
+			local meanDist = GetMeanDist(uCol, uRow, builderTeam)
+			taxValue = GetTaxValue(meanDist)
+			RegisterCellTaxValue(uCol, uRow, builderTeam, taxValue)
+		end
 		return ProcessBuildStep(builderID, builderTeam, unitID, unitDefID, part, taxValue)
 	else
 		return true
 	end
 end
-	
+
+-- Some unsynced stuff for perf tests (== full map map iterations)
+-- local PendingChanges = {}
+
+-- function gadget:Update()
+	-- local MyTeam = Spring.GetMyTeamID()
+	-- startCell = lastCell and (lastCell + 1) or 1
+	-- for n = startCell, (startCell + 64) do
+		-- lastCell = n
+		-- local currentCellx, currentCellz = n%nCol, math.floor(n/nRows)
+		-- local meanDist = GetMeanDist(currentCellx, currentCellz, MyTeam)
+		-- local taxValue = GetTaxValue(meanDist)
+		-- table.insert(PendingChanges, {x = currentCellx, z = currentCellz, tax = taxValue})
+		-- if lastCell == nCells then
+			-- break
+		-- end
+	-- end
+	-- if lastCell == nCells then
+		-- lastCell = 0
+	-- end
+-- end
+
+-- function gadget:DrawGenesis()
+	-- if #PendingChanges > 0 then
+		-- for i = 1, #PendingChanges do
+			-- Spring.Echo(PendingChanges[i].x,PendingChanges[i].z,PendingChanges[i].tax)
+		-- end
+		-- PendingChanges = {}
+	-- end
+-- end
