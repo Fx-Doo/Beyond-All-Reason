f235597567b6ddd027312415b56e8cb2a4b6c65d
Fx-Doo <hedibertille93@gmail.com>
2025-10-16 21:59:12 +0200

    Handle taxing overflow + add "1.0" tax to disable sharing whole
    Sorry if this is not well made
    I tried to get something to work as a proof of concept; and maybe get us the ability to test a "no sharing mode"
    This doesn't handle "exploits" via unit/feature reclaiming (although i already proposed means to handle those in a disc post)

--- luaui/Widgets/gui_top_bar.lua
+++ luaui/Widgets/gui_top_bar.lua
@@ -2021,7 +2021,7 @@ function widget:MousePress(x, y, button)
 		end
 
 		if not spec then
-			if not isSingle then
+			if not isSingle  then
 				if math_isInRect(x, y, shareIndicatorArea['metal'][1], shareIndicatorArea['metal'][2], shareIndicatorArea['metal'][3], shareIndicatorArea['metal'][4]) then
 					draggingShareIndicator = 'metal'
 				end
@@ -2145,7 +2145,7 @@ function widget:Initialize()
 
 	if not spec then
 		local teamList = Spring.GetTeamList(myAllyTeamID) or {}
-		isSingle = #teamList == 1
+		isSingle = (#teamList == 1) or (Spring.GetModOptions().tax_resource_sharing_amount and Spring.GetModOptions().tax_resource_sharing_amount == 1)
 	end
 
 	for unitDefID, unitDef in pairs(UnitDefs) do
